7897388cd1ccbc3429482f841e148d52d14e3199
El Capitan <heneverloses@gmail.com>
2021-09-30 02:24:05 -0500

    Formato de nombres

---
+++ 10_cadenas/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main () 
+{
+    char * cadena1 = " hola1 ";
+    printf (" Valor de cadena1 : %s\n", cadena1 );
+    char cadena2 [] = " hola2 ";
+    printf (" Valor de cadena2 : %p\n", cadena2 );
+}
